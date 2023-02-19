fd209c561a9c569c91d522d498498821feff4f4c
Thomas <thomai@posteo.de>
2023-02-19 10:32:47 +0100

    Add cicd

--- src/components/ColorSelection.tsx
+++ src/components/ColorSelection.tsx
@@ -2,14 +2,34 @@ type Props = {
   onSelectColor: (color: string) => void
 }
 
+type Color = { text: string, value: string }
+
 export const ColorSelection = ({ onSelectColor }: Props) => {
 
-  const colors = ['red', 'green', 'blue', 'violet', 'white', 'brown', 'black']
+  const colors = [
+    { text: 'Rot', value: 'red' },
+    { text: 'Grün', value: 'green' },
+    { text: 'Blau', value: 'blue' },
+    { text: 'Lila', value: '#6C3483' },
+    { text: 'Oransch', value: '#FF6400' },
+    { text: 'Weiß', value: 'white' },
+    { text: 'Braun', value: '#6E2C00' },
+    { text: 'Schwarz', value: 'black' },
+  ]
+
+  const onSelect = (color: Color) => {
+    onSelectColor(color.value)
+
+    const u = new SpeechSynthesisUtterance()
+    u.text = color.text
+    u.lang = 'de'
+    window.speechSynthesis.speak(u)
+  }
 
   return (
     <div>
       {colors.map(color => (
-        <button key={color} style={{ backgroundColor: color, width: '80px', height: '80px', margin: '3px' }} onClick={() => onSelectColor(color)}></button>
+        <button key={color.value} style={{ backgroundColor: color.value, width: '80px', height: '80px', margin: '3px', border: '1px solid white' }} onClick={() => onSelect(color)}></button>
       ))}
     </div>
   )
