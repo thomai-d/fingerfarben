fd4ec1b81ffd664fe5785d07ff65c8dfe4424589
Thomas <thomai@posteo.de>
2023-02-19 09:39:56 +0100

    Drawing works

---
+++ src/components/Canvas.ts
@@ -0,0 +1,99 @@
+import { TouchEvent, MouseEvent } from "react"
+
+export class Canvas {
+
+  private context: CanvasRenderingContext2D
+
+  private lastMouseEvent: MouseEvent | null = null
+
+  private touches: Map<number, { id: number, x: number, y: number }> = new Map()
+  private color: string = 'red'
+
+  constructor(private canvas: HTMLCanvasElement) {
+    this.context = canvas.getContext('2d')!
+    console.debug("Initialized canvas")
+  }
+
+  resize(width: number, height: number) {
+    console.debug(`Resizing canvas to ${width}x${height}`)
+
+    let temp = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height)
+    this.canvas.width = width
+    this.canvas.height = height
+    this.context.putImageData(temp, 0, 0)
+
+    this.context.fillStyle = 'white'
+  }
+
+  onMouseDown = (event: MouseEvent) => {
+    this.lastMouseEvent = event
+  }
+
+  onMouseMove = (event: MouseEvent) => {
+    if (!this.lastMouseEvent)
+      return
+
+    this.drawLine(this.toRelativeXY(this.lastMouseEvent), this.toRelativeXY(event))
+    this.lastMouseEvent = event
+  }
+
+  onMouseUp = (event: MouseEvent) => {
+    this.lastMouseEvent = null
+  }
+
+  drawLine = (start: { x: number, y: number }, end: { x: number, y: number }) => {
+    this.context.lineWidth = 8
+    this.context.beginPath();
+    this.context.moveTo(start.x, start.y);
+    this.context.lineTo(end.x, end.y);
+    this.context.strokeStyle = this.color;
+    this.context.stroke();
+  };
+
+  onTouchStart = (event: TouchEvent) => {
+    const newTouches = Array.from(event.changedTouches).map(touch => ({
+      id: touch.identifier,
+      x: touch.clientX,
+      y: touch.clientY,
+    }));
+    newTouches.forEach(touch => {
+      this.touches.set(touch.id, touch);
+    });
+  };
+
+  onTouchMove = (event: TouchEvent) => {
+    const updatedTouches = Array.from(event.changedTouches).map(touch => ({
+      id: touch.identifier,
+      x: touch.clientX,
+      y: touch.clientY,
+    }));
+    updatedTouches.forEach(updatedTouch => {
+      const previousTouch = this.touches.get(updatedTouch.id);
+      if (previousTouch) {
+        this.drawLine(this.touchToXy(previousTouch), this.touchToXy(updatedTouch));
+      }
+      this.touches.set(updatedTouch.id, updatedTouch);
+    });
+  };
+
+  onTouchEnd = (event: TouchEvent) => {
+    const endedTouches = Array.from(event.changedTouches).map(touch => touch.identifier);
+    endedTouches.forEach(id => {
+      this.touches.delete(id);
+    });
+  };
+
+  private toRelativeXY(e: { pageX: number, pageY: number }) {
+    return {
+      x: e.pageX - this.canvas.offsetLeft,
+      y: e.pageY - this.canvas.offsetTop
+    }
+  }
+
+  private touchToXy(e: { x: number, y: number }) {
+    return {
+      x: e.x - this.canvas.offsetLeft,
+      y: e.y - this.canvas.offsetTop
+    }
+  }
+}
--- src/components/ResizableCanvas.tsx
+++ src/components/ResizableCanvas.tsx
@@ -1,37 +1,18 @@
 import { memo, useEffect, useRef } from "react"
 import { useResizeObserver } from "../hooks/useResizeObserver"
-
-class MyCanvas {
-
-  private context: CanvasRenderingContext2D
-
-  constructor(private canvas: HTMLCanvasElement) {
-    this.context = canvas.getContext('2d')!
-    console.debug("Initialized canvas")
-  }
-
-  resize(width: number, height: number) {
-    console.debug(`Resizing canvas to ${width}x${height}`)
-    this.canvas.width = width
-    this.canvas.height = height
-
-    this.context.fillStyle = 'white'
-    this.context.fillRect(10, 10, width - 20, height - 20)
-  }
-}
-
+import { Canvas } from './Canvas'
 
 export const ResizableCanvas = memo(() => {
   const divRef = useRef<HTMLDivElement>(null)
   const canvasElemRef = useRef<HTMLCanvasElement>(null)
-  const canvasRef = useRef<MyCanvas | null>(null)
+  const canvasRef = useRef<Canvas | null>(null)
 
   useEffect(() => {
     if (!divRef.current || !canvasElemRef.current) {
       return;
     }
 
-    canvasRef.current = new MyCanvas(canvasElemRef.current)
+    canvasRef.current = new Canvas(canvasElemRef.current)
 
   }, [divRef])
 
@@ -46,7 +27,15 @@ export const ResizableCanvas = memo(() => {
 
   return (
     <div ref={divRef} style={{ flex: 1, overflow: 'hidden' }}>
-      <canvas ref={canvasElemRef} />
+      <canvas
+        ref={canvasElemRef}
+        onMouseDown={e => canvasRef.current?.onMouseDown(e)}
+        onMouseMove={e => canvasRef.current?.onMouseMove(e)}
+        onMouseUp={e => canvasRef.current?.onMouseUp(e)}
+        onTouchStart={e => canvasRef.current?.onTouchStart(e)}
+        onTouchMove={e => canvasRef.current?.onTouchMove(e)}
+        onTouchEnd={e => canvasRef.current?.onTouchEnd(e)}
+      />
     </div>
   )
 })
